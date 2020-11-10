32f21b99eed1a5eda7e272c883e66d4c935711e5
Alexandre A. Scrocaro Junior <49293989+Scrocaro@users.noreply.github.com>
2020-11-10 17:07:25 -0300

    Add files via upload

--- 3 semestre/BD/PROJETO/parte 2/tabelas_locadora_veiculos.sql
+++ 3 semestre/BD/PROJETO/parte 2/tabelas_locadora_veiculos.sql
@@ -17,7 +17,7 @@ DROP TABLE IF EXISTS CATEGORIA_VEICULO;
 
 CREATE TABLE CATEGORIA_VEICULO(
     categoria VARCHAR(1),
-    nome VARCHAR(200),
+    nome VARCHAR(100),
     preco_dia DECIMAL(10, 2),
     combustivel DECIMAL(10, 2),
     seguro DECIMAL(10, 2),
@@ -26,7 +26,7 @@ CREATE TABLE CATEGORIA_VEICULO(
 
 CREATE TABLE VEICULO(
     placa VARCHAR(9),
-    categoria_v VARCHAR(1),
+    categoria_veiculo VARCHAR(1),
     nome VARCHAR(50),
     cor VARCHAR(20),
     lugares INTEGER,
@@ -34,23 +34,23 @@ CREATE TABLE VEICULO(
     tipo_combustivel VARCHAR(20),
     disponibilidade VARCHAR(8),
     PRIMARY KEY (placa),
-	FOREIGN KEY (categoria_v) REFERENCES CATEGORIA_VEICULO (categoria)
+	FOREIGN KEY (categoria_veiculo) REFERENCES CATEGORIA_VEICULO (categoria)
 );
 
 CREATE TABLE VEICULO_CARGA(
     placa_veiculo VARCHAR(9),
-    categoria_v VARCHAR(1),
+    categoria_veiculo VARCHAR(1),
 	peso_max_ton DECIMAL(10,2),
     altura DECIMAL(10,2),
 	comprimento DECIMAL(10,2),
-    PRIMARY KEY (placa_veiculo, categoria_v, peso_max_ton),
+    PRIMARY KEY (placa_veiculo, categoria_veiculo, peso_max_ton),
 	FOREIGN KEY (placa_veiculo) REFERENCES VEICULO (placa),
-    FOREIGN KEY (categoria_v) REFERENCES CATEGORIA_VEICULO (categoria)
+    FOREIGN KEY (categoria_veiculo) REFERENCES CATEGORIA_VEICULO (categoria)
 );
 
 CREATE TABLE CLIENTE(
 	id INTEGER,	
-    nome VARCHAR(200),
+    nome VARCHAR(100),
     telefone VARCHAR(20),
     email VARCHAR(50),
     PRIMARY KEY (id)
@@ -58,17 +58,17 @@ CREATE TABLE CLIENTE(
 
 CREATE TABLE PESSOA_FISICA(
 	id_cliente INTEGER,
-	cpf VARCHAR(50),
-    rg VARCHAR(50),
+	cpf VARCHAR(15),
+    rg VARCHAR(15),
     sexo ENUM ('M', 'F', 'O'),	-- Masculino, Feminino, Outro
-    data_nascimento DATE,
+    idade VARCHAR(3),
     PRIMARY KEY (id_cliente, cpf),
     FOREIGN KEY (id_cliente) REFERENCES CLIENTE (id) 
 );
 
 CREATE TABLE PESSOA_JURIDICA(
 	id_cliente INTEGER,
-	cnpj VARCHAR(50),
+	cnpj VARCHAR(20),
     razao_social VARCHAR(50),
     tipo_organizacao VARCHAR(50),
     PRIMARY KEY (id_cliente, cnpj),
@@ -83,26 +83,26 @@ CREATE TABLE CARGO(
 );
 
 CREATE TABLE FUNCIONARIO(
-	cpf_funcionario VARCHAR(50),
+	cpf VARCHAR(15),
     id_cargo INTEGER,
-    rg VARCHAR(50),
-    nome VARCHAR(200),
+    rg VARCHAR(15),
+    nome VARCHAR(100),
     idade VARCHAR(3),
     sexo ENUM ('M', 'F', 'O'),		
-    PRIMARY KEY (cpf_funcionario),
+    PRIMARY KEY (cpf),
     FOREIGN KEY (id_cargo) REFERENCES CARGO (id)
 );
 
 CREATE TABLE DEPENDENTES(
-	cpf_funcion VARCHAR(50),
-    id_cargo_funcion INTEGER,
-    nome VARCHAR(200),
+	cpf_funcionario VARCHAR(15),
+    id_cargo_funcionario INTEGER,
+    nome VARCHAR(100),
 	idade VARCHAR(3),
     custo DECIMAL(10, 2),
     relacionamento VARCHAR(20),
-    PRIMARY KEY (cpf_funcion, nome),
-    FOREIGN KEY (id_cargo_funcion) REFERENCES FUNCIONARIO (id_cargo),
-    FOREIGN KEY (cpf_funcion) REFERENCES FUNCIONARIO (cpf_funcionario) ON DELETE CASCADE
+    PRIMARY KEY (cpf_funcionario, nome),
+    FOREIGN KEY (id_cargo_funcionario) REFERENCES FUNCIONARIO (id_cargo),
+    FOREIGN KEY (cpf_funcionario) REFERENCES FUNCIONARIO (cpf) ON DELETE CASCADE
 );
 
 CREATE TABLE REVISAO(
@@ -127,12 +127,12 @@ CREATE TABLE ENDERECO_CLIENTE(
 CREATE TABLE LOCACAO(
 	id INTEGER,
     id_cliente INTEGER,
-    id_funcionario VARCHAR(50),
+    cpf_funcionario VARCHAR(15),
 	placa_veiculo VARCHAR(9),
     data_inicio DATE,
     data_termino DATE,
-    PRIMARY KEY (id, id_cliente, id_funcionario, placa_veiculo),
+    PRIMARY KEY (id, id_cliente, cpf_funcionario, placa_veiculo),
 	FOREIGN KEY (id_cliente) REFERENCES CLIENTE(id),
-    FOREIGN KEY (id_funcionario) REFERENCES FUNCIONARIO(cpf_funcionario),
+    FOREIGN KEY (cpf_funcionario) REFERENCES FUNCIONARIO(cpf),
     FOREIGN KEY (placa_veiculo) REFERENCES VEICULO(placa)
 );
